47a7cf0622779c168a2121a2474cf7f491572e2e
Sodvadiyavidhi <200429730@student.georgianc.on.ca>
2020-06-17 16:34:37 -0400

    first commit

---
+++ controllers/ProductsController.js
@@ -0,0 +1,26 @@
+const viewPath = ('products');
+const Product = require('../models/product');
+
+exports.show = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.render(`${viewPath}/show`,{
+    pageTitle: product.name,
+    product: product
+  });
+};
+
+exports.new = (req, res) => {
+  res.render(`${viewPath}/new`, {
+    pageTitle: 'New Product'
+  });
+};
+
+exports.create = async (req, res) => {
+try{
+  const product = await Product.create(req.body);
+  res.redirect(`/products/show`);
+}
+catch(err){
+  console.log(err);
+}
+};
